ce25bedb1c1f873eb1a842a5629601c15747f9ac
mukeshkumargiri523 <mukeshkumargiri523@gmail.com>
2023-12-10 21:28:59 +0530

    pagenotfound fixed

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,6 +1,16 @@
 import "./Style.scss";
-function PageNotFound() {
-  return <div>PageNotFound</div>;
-}
+
+import ContentWrapper from "../../contentWrapper/ContentWrapper";
+
+const PageNotFound = () => {
+  return (
+    <div className="pageNotFound">
+      <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page Not Found</span>
+      </ContentWrapper>
+    </div>
+  );
+};
 
 export default PageNotFound;
